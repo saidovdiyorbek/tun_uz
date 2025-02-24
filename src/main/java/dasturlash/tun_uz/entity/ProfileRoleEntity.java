8db0dac22934453cffea8232ead044103742ff3c
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-24 17:43:35 +0500

    Added: ProfileRoleEntity and fields

---
+++ src/main/java/dasturlash/tun_uz/entity/ProfileRoleEntity.java
@@ -0,0 +1,34 @@
+package dasturlash.tun_uz.entity;
+
+import dasturlash.tun_uz.enums.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "profile_role")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileRoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "profile_id", nullable = false)
+    Integer profileId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", insertable = false, updatable = false)
+    Profile profile;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Column(nullable = false)
+    LocalDateTime createdDate;
+}
