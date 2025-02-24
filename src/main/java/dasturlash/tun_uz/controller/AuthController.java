d93a829d938bde17a17b77ad9acd519e3b6ac989
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-24 17:44:11 +0500

    Added: AuthController

---
+++ src/main/java/dasturlash/tun_uz/controller/AuthController.java
@@ -0,0 +1,10 @@
+package dasturlash.tun_uz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+}
