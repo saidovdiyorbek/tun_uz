0246a6e0d3cd57ecc4be6da67af1ac58b5da517d
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-25 09:12:52 +0500

    Added: ProfileRepository.java

---
+++ src/main/java/dasturlash/tun_uz/repository/ProfileRepository.java
@@ -0,0 +1,7 @@
+package dasturlash.tun_uz.repository;
+
+import dasturlash.tun_uz.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+}
