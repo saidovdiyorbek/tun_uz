53a59606ad0bcb035ba0bda3bb422ce3603c97a0
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-25 14:53:28 +0500

    Added: APIs registration and verification

--- src/main/java/dasturlash/tun_uz/controller/AuthController.java
+++ src/main/java/dasturlash/tun_uz/controller/AuthController.java
@@ -1,10 +1,31 @@
 package dasturlash.tun_uz.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dasturlash.tun_uz.dto.profile.RegistrationDTO;
+import dasturlash.tun_uz.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> register(@RequestBody @Valid RegistrationDTO dto) {
+        return ResponseEntity.ok(authService.registration(dto));
+    }
+
+    @GetMapping("/verification/{code}")
+    public ResponseEntity<String> verification(@PathVariable("code") int code) {
+        return ResponseEntity.ok(authService.verification(code));
+    }
 
 }
