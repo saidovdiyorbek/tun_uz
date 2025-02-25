c6f949e10c497d313d66eb81c7293779c1c01252
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-02-25 15:02:24 +0500

    Added: entity EmailSentHistory

---
+++ src/main/java/dasturlash/tun_uz/entity/EmailSentHistory.java
@@ -0,0 +1,35 @@
+package dasturlash.tun_uz.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "email_sent_history")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EmailSentHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    int code;
+
+    @Column(nullable = false)
+    LocalDateTime sentDate;
+
+    @Column(nullable = false)
+    LocalDateTime expiredDate;
+
+    @Column(nullable = false)
+    boolean expired;
+}
