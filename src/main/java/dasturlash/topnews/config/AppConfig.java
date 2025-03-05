c5761b51ac8e54e9563072e019fa366b0f7079ef
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-05 16:40:30 +0500

    Added: AppConfig.java

---
+++ src/main/java/dasturlash/topnews/config/AppConfig.java
@@ -0,0 +1,21 @@
+package dasturlash.topnews.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(new Locale("UZ"));
+        return messageSource;
+    }
+}
