c3dc33788de983f146cea4357642770fa4460aee
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-05 16:29:48 +0500

    Added: ProfileRoleEntity

--- src/main/java/dasturlash/topnews/entity/ProfileRoleEntity.java
+++ src/main/java/dasturlash/topnews/entity/ProfileRoleEntity.java
@@ -1,6 +1,6 @@
-package dasturlash.tun_uz.entity;
+package dasturlash.topnews.entity;
 
-import dasturlash.tun_uz.enums.Role;
+import dasturlash.topnews.enums.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
