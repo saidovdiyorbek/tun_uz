c914d773aa98468e6c7cce55ad06c23f49da0b58
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-05 16:42:05 +0500

    Added: CustomGlobalExceptionHandler.java

--- src/main/java/dasturlash/topnews/handler/CustomGlobalExceptionHandler.java
+++ src/main/java/dasturlash/topnews/handler/CustomGlobalExceptionHandler.java
@@ -1,10 +1,13 @@
-package dasturlash.tun_uz.handler;
+package dasturlash.topnews.handler;
 
+import dasturlash.topnews.exceptions.CheckVerificationCodeException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,4 +33,14 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         body.put("errors", errors);
         return new ResponseEntity<>(body, headers, status);
     }
+
+        @ExceptionHandler(CheckVerificationCodeException.class)
+        public ResponseEntity<Object> exceptionHandler(CheckVerificationCodeException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+
+        @ExceptionHandler(UsernameNotFoundException.class)
+        public ResponseEntity<Object> exceptionHandler(UsernameNotFoundException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
 }
