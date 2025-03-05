284f811705d7ff97400f3a8a704a164dd51a89ac
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-05 16:29:32 +0500

    Added: ProfileRoleEntityRepository

---
+++ src/main/java/dasturlash/topnews/repository/ProfileRoleEntityRepository.java
@@ -0,0 +1,8 @@
+package dasturlash.topnews.repository;
+
+import dasturlash.topnews.entity.ProfileRoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRoleEntityRepository extends JpaRepository<ProfileRoleEntity, Integer> {
+
+}
