ff23750b380eaf9f632f38dc1b07f9de0c03a7c9
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-05 16:41:46 +0500

    Added: custom exception

---
+++ src/main/java/dasturlash/topnews/exceptions/CheckVerificationCodeException.java
@@ -0,0 +1,7 @@
+package dasturlash.topnews.exceptions;
+
+public class CheckVerificationCodeException extends RuntimeException {
+    public CheckVerificationCodeException(String message) {
+        super(message);
+    }
+}
