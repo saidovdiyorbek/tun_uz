8df4dd5bb9bb4eeff554457d13fea67bcb1f3684
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-08 11:35:11 +0500

    Added: Email sending messages change format

--- src/main/java/dasturlash/topnews/repository/EmailSentHistoryRepository.java
+++ src/main/java/dasturlash/topnews/repository/EmailSentHistoryRepository.java
@@ -1,8 +1,12 @@
 package dasturlash.topnews.repository;
 
 import dasturlash.topnews.entity.EmailSentHistory;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface EmailSentHistoryRepository extends JpaRepository<EmailSentHistory, String> {
 
@@ -12,4 +16,10 @@ public interface EmailSentHistoryRepository extends JpaRepository<EmailSentHisto
             "from EmailSentHistory e " +
             "where e.code = ?1 and e.expired = false")
     EmailSentHistory findByCode(int code);
+
+    @Query(
+            "from EmailSentHistory e " +
+            "where e.email = ?1 and e.expired = false")
+    EmailSentHistory findByEmail(String email);
+
 }
