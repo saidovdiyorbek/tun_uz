0299cc035425753fe7c234fc89b4afb9f96a6a8a
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-13 00:41:01 +0500

    Added: login service method

--- src/main/java/dasturlash/topnews/service/AuthService.java
+++ src/main/java/dasturlash/topnews/service/AuthService.java
@@ -1,14 +1,24 @@
 package dasturlash.topnews.service;
 
+import dasturlash.topnews.config.CustomUserDetails;
+import dasturlash.topnews.dto.PhotoResponse;
 import dasturlash.topnews.dto.StandardResponse;
 import dasturlash.topnews.dto.profile.RegistrationDTO;
 import dasturlash.topnews.entity.Profile;
 import dasturlash.topnews.enums.AppLanguage;
 import dasturlash.topnews.enums.ProfileStatus;
+import dasturlash.topnews.exceptions.AppBadException;
 import dasturlash.topnews.service.emailService.EmailSendingService;
 import dasturlash.topnews.service.emailService.EmailSentHistoryService;
+import dasturlash.topnews.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthService {
@@ -16,16 +26,25 @@ public class AuthService {
     private final EmailSentHistoryService emailSentHistoryService;
     private final ResourceBundleService resourceBundleService;
     private final ProfileService profileService;
+    private final AuthenticationManager authenticationManager;
+    private final ProfileRoleEntityService  profileRoleEntityService;
+    private final AttachService  attachService;
 
     @Autowired
     public AuthService(EmailSendingService emailSendingService,
                        EmailSentHistoryService emailSentHistoryService,
                        ResourceBundleService resourceBundleService,
-                       ProfileService profileService) {
+                       ProfileService profileService,
+                       AuthenticationManager authenticationManager,
+                       ProfileRoleEntityService profileRoleEntityService,
+                       AttachService attachService) {
         this.emailSendingService = emailSendingService;
         this.emailSentHistoryService = emailSentHistoryService;
         this.resourceBundleService = resourceBundleService;
         this.profileService = profileService;
+        this.authenticationManager = authenticationManager;
+        this.profileRoleEntityService = profileRoleEntityService;
+        this.attachService = attachService;
     }
 
     public String registration(RegistrationDTO dto, AppLanguage lang) {
@@ -61,6 +80,28 @@ public class AuthService {
     }
 
 
+    public RegistrationDTO.LoginDTO.LoginResponse login(RegistrationDTO.LoginDTO dto, AppLanguage lang) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.email(), dto.password()));
+            if (authentication.isAuthenticated()) {
+                CustomUserDetails profile = (CustomUserDetails) authentication.getPrincipal();
+                String token = JwtUtil.encode(profile.getUsername(), profile.getId(), profile.getRole());
 
+                List<String> profileRoles = profileRoleEntityService.getRolesByProfileId(profile.getId());
+                Profile currentUser = profileService.getById(profile.getId());
 
+                attachService.openUrl(currentUser.getPhotoId());
+                PhotoResponse photoResponse = new PhotoResponse(currentUser.getPhotoId(), attachService.openUrl(currentUser.getPhotoId()));
+
+                return new RegistrationDTO.LoginDTO.LoginResponse(profile.getName(), currentUser.getSurname(), profile.getUsername(), profileRoles, photoResponse);
+            }
+            throw new AppBadException("Invalid email or password");
+        }catch (BadCredentialsException e){
+            throw new AppBadException("Invalid email or password");
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new AppBadException("Internal server error");
+        }
+    }
 }
