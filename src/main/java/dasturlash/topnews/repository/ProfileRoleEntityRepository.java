f69b485a511e76855af8a22da186a924825713ce
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-13 00:43:45 +0500

    Added: query get roles name by profile id

--- src/main/java/dasturlash/topnews/repository/ProfileRoleEntityRepository.java
+++ src/main/java/dasturlash/topnews/repository/ProfileRoleEntityRepository.java
@@ -2,7 +2,14 @@ package dasturlash.topnews.repository;
 
 import dasturlash.topnews.entity.ProfileRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProfileRoleEntityRepository extends JpaRepository<ProfileRoleEntity, Integer> {
-
+    @Query(value = "select STRING_AGG(pr.roles, ',') roles " +
+            "from profile_role pr " +
+            "where pr.profile_id = ?1 " +
+            "group by pr.roles", nativeQuery = true)
+    List<String> getRolesByProfileId(Integer profileId);
 }
