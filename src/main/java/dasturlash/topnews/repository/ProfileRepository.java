b05fea26b002e6b94c55ac1468b2fc3560337697
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-13 00:43:17 +0500

    Added: add query method find profile

--- src/main/java/dasturlash/topnews/repository/ProfileRepository.java
+++ src/main/java/dasturlash/topnews/repository/ProfileRepository.java
@@ -3,7 +3,10 @@ package dasturlash.topnews.repository;
 import dasturlash.topnews.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
 
     Profile findByEmailAndVisibleTrue(String username);
+    Optional<Profile> findByIdAndVisibleTrue(Integer profileId);
 }
