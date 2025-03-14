075d369a13c2daefc45223b308020ae0231deadc
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-14 18:09:20 +0500

    Added: fix column name

--- src/main/java/dasturlash/topnews/repository/ProfileRoleEntityRepository.java
+++ src/main/java/dasturlash/topnews/repository/ProfileRoleEntityRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProfileRoleEntityRepository extends JpaRepository<ProfileRoleEntity, Integer> {
-    @Query(value = "select STRING_AGG(pr.roles, ',') roles " +
+    @Query(value = "select STRING_AGG(pr.role, ',') roles " +
             "from profile_role pr " +
             "where pr.profile_id = ?1 " +
-            "group by pr.roles", nativeQuery = true)
+            "group by pr.role", nativeQuery = true)
     List<String> getRolesByProfileId(Integer profileId);
 }
