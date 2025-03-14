d51aad2680dff0b8ba2b94cf074de3b31cd7b8a3
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-03-14 18:07:33 +0500

    Added: delete fields

--- src/main/java/dasturlash/topnews/util/JwtUtil.java
+++ src/main/java/dasturlash/topnews/util/JwtUtil.java
@@ -1,55 +1,65 @@
-package dasturlash.topnews.util;
+    package dasturlash.topnews.util;
 
-import dasturlash.topnews.dto.JwtDTO;
-import dasturlash.topnews.entity.ProfileRoleEntity;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
+    import dasturlash.topnews.dto.JwtDTO;
+    import dasturlash.topnews.entity.ProfileRoleEntity;
+    import io.github.cdimascio.dotenv.Dotenv;
+    import io.jsonwebtoken.Claims;
+    import io.jsonwebtoken.Jwts;
+    import io.jsonwebtoken.io.Decoders;
+    import io.jsonwebtoken.security.Keys;
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+    import javax.crypto.SecretKey;
+    import java.util.Date;
+    import java.util.HashMap;
+    import java.util.Map;
+    @Component
+    public class JwtUtil {
+        private final String secretKey;
+        private final int tokenLiveTime;
 
-public class JwtUtil {
-    @Value("${jwt.secret.key}")
-    private static String secretKey;
-    @Value("${jwt.token.live.time}")
-    private static int tokenLiveTime;
+        static Dotenv dotenv = Dotenv.configure()
+                .filename("secret.env")
+                .directory("/media/diyorbek/HDD 1 TB/IntellIj'/level-up/topnews_project/topnews")
+                .load();
 
-    public static String encode(String username, Integer id, ProfileRoleEntity roles){
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("username", username);
-        claims.put("id", String.valueOf(id));
-        claims.put("roles", roles.getRole().name());
+        public JwtUtil() {
+            this.secretKey = dotenv.get("JWT_SECRET_KEY");
+            this.tokenLiveTime = Integer.parseInt(dotenv.get("JWT_TOKEN_LIVE_TIME"));
+        }
 
-        return Jwts
-                .builder()
-                .claims(claims)
-                .subject(username)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
-                .signWith(getSignInKey())
-                .compact();
+        public  String encode(String username, Integer id, ProfileRoleEntity roles){
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("username", username);
+            claims.put("id", String.valueOf(id));
+            claims.put("roles", roles.getRole().name());
+
+            return Jwts
+                    .builder()
+                    .claims(claims)
+                    .subject(username)
+                    .issuedAt(new Date(System.currentTimeMillis()))
+                    .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
+                    .signWith(getSignInKey())
+                    .compact();
+        }
+
+        public  JwtDTO decode(String token) {
+            Claims claims = Jwts
+                    .parser()
+                    .verifyWith(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getPayload();
+            String username = claims.get("username").toString();
+            String role = (String) claims.get("roles");
+            return new JwtDTO(username, role);
+
+        }
+
+        private  SecretKey getSignInKey(){
+            byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+            return Keys.hmacShaKeyFor(keyBytes);
+        }
     }
-
-    public static JwtDTO decode(String token) {
-        Claims claims = Jwts
-                .parser()
-                .verifyWith(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getPayload();
-        String username = claims.get("username").toString();
-        String role = (String) claims.get("roles");
-        return new JwtDTO(username, role);
-
-    }
-
-    private static SecretKey getSignInKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-}
